1beef33096ea8d13fbb398e7271eea9a57a4f945
Chibgatullah Minhaz <chibgatullahminhaz@gmail.com>
2024-09-16 16:41:18 +0600

    responsive done

--- scritp.js
+++ scritp.js
@@ -3,21 +3,22 @@ const inputFields = document.querySelectorAll(".goal_input");
 const progressbar = document.querySelector('.progress-bar');
 const progressValue= document.querySelector('.progress-value');
 
+const allgoals = {};
+
 checkboxs.forEach((checked) => {
     checked.addEventListener("click", () => {
-        const goapChecked = [...inputFields].every((e) => {
+        const goapChecked = [...inputFields].every((e) => { //converting inputFeild in a new array
             console.log(e.value)
             return e.value;
         })
         if (goapChecked) {
             checked.parentElement.classList.toggle('completed');
-            progressValue.style.width = '33.33%';
         } else {
             progressbar.classList.add('show-error')
         }
     })
 })
-inputFields.forEach((input)=>{
+inputFields.forEach((input)=>{ // focusing on inputFeilds
     input.addEventListener("focus", ()=>{
         progressbar.classList.remove('show-error')
     })
